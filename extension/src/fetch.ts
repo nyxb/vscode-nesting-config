c7d6b93ebabeac189858bbe5bd178fd3f71cc1af
nyxb <contact@nyxb.xyz>
2023-06-09 10:44:56 +0200

    🆕 feat(fetch.ts): add fetchLatest function to fetch the latest configuration from a remote repository and parse it 🆕 feat(fetch.ts): add fetchAndUpdate function to fetch the latest configuration and update the local configuration if it has changed 🆕 feat(index.ts): add activate function to register the manualUpdate command and initialize the extension by fetching the latest configuration 🆕 feat(index.ts): add deactivate function to deactivate the extension when it is uninstalled or disabled

---
+++ extension/src/fetch.ts
@@ -0,0 +1,65 @@
+import fetch from 'node-fetch'
+import type { ExtensionContext } from 'vscode'
+import { window, workspace } from 'vscode'
+import { FILE, MSG_PREFIX, URL_PREFIX } from './constants'
+import { getConfig } from './config'
+
+export async function fetchLatest() {
+   const repo = getConfig<string>('nestingConfigUpdater.upstreamRepo')
+   const branch = getConfig<string>('nestingConfigUpdater.upstreamBranch')
+   const url = `${URL_PREFIX}/${repo}@${branch}/${FILE}`
+   const md = await fetch(url).then(r => r.text())
+   const content = (md.match(/```jsonc([\s\S]*?)```/) || [])[1] || ''
+
+   const json = `{${
+    content
+      .trim()
+      .split(/\n/g)
+      .filter(line => !line.trim().startsWith('//'))
+      .join('\n')
+      .slice(0, -1)
+  }}`
+
+   const config = JSON.parse(json) || {}
+   return config['explorer.fileNesting.patterns']
+}
+
+export async function fetchAndUpdate(ctx: ExtensionContext, prompt = true) {
+   const config = workspace.getConfiguration()
+   const patterns = await fetchLatest()
+   let shouldUpdate = true
+
+   const oringalPatterns = { ...(config.get<object>('explorer.fileNesting.patterns') || {}) }
+   delete oringalPatterns['//']
+   // no change
+   if (Object.keys(oringalPatterns).length > 0 && JSON.stringify(patterns) === JSON.stringify(oringalPatterns))
+      return false
+
+   if (prompt) {
+      const buttonUpdate = 'Update'
+      const buttonSkip = 'Skip this time'
+      const result = await window.showInformationMessage(
+      `${MSG_PREFIX} new config found, do you want to update?`,
+      buttonUpdate,
+      buttonSkip,
+      )
+      shouldUpdate = result === buttonUpdate
+   }
+
+   if (shouldUpdate) {
+      if (config.inspect('explorer.fileNesting.enabled')?.globalValue == null)
+         config.update('explorer.fileNesting.enabled', true, true)
+
+      if (config.inspect('explorer.fileNesting.expand')?.globalValue == null)
+         config.update('explorer.fileNesting.expand', false, true)
+
+      config.update('explorer.fileNesting.patterns', {
+         '//': `Last update at ${new Date().toLocaleString()}`,
+         ...patterns,
+      }, true)
+
+      ctx.globalState.update('lastUpdate', Date.now())
+
+      window.showInformationMessage(`${MSG_PREFIX} Config updated`)
+   }
+}
---
+++ extension/src/index.ts
@@ -0,0 +1,24 @@
+import type { ExtensionContext } from 'vscode'
+import { commands } from 'vscode'
+import { getConfig } from './config'
+import { fetchAndUpdate } from './fetch'
+
+export async function activate(ctx: ExtensionContext) {
+   commands.registerCommand('nyxb.nesting-config.manualUpdate', () => fetchAndUpdate(ctx, false))
+
+   const lastUpdate = ctx.globalState.get('lastUpdate', 0)
+   const initialized = ctx.globalState.get('init', false)
+   const autoUpdateInterval = getConfig<number>('nestingConfigUpdater.autoUpdateInterval')
+
+   if (!initialized) {
+      ctx.globalState.update('init', true)
+      fetchAndUpdate(ctx, false)
+   }
+
+   if (getConfig('nestingConfigUpdater.autoUpdate')) {
+      if (Date.now() - lastUpdate >= autoUpdateInterval * 60_000)
+         fetchAndUpdate(ctx, getConfig('nestingConfigUpdater.promptOnAutoUpdate'))
+   }
+}
+
+export function deactivate() {}
